960fed14d00145205cc480d633e12b1627562c03
calamis <mekorn_damboy@yahoo.com>
2022-01-24 22:12:45 +0800

    chore: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 const root = resolve(__dirname, 'src');
-const outDir = resolve(__dirname, 'dist')
+const outDir = resolve(__dirname, 'dist');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root,
   plugins: [react()],
-  build: {
-    outDir,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(root, 'index.html'),
-        home: resolve(root, 'pages/home/index.html'),
-        about: resolve(root,  'pages/about/index.html'),
-        profile: resolve(root, 'pages/profile/index.html')      }
-    }
-  }
-})
+  // build: {
+  //   outDir,
+  //   emptyOutDir: true,
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(root, 'index.html'),
+  //       home: resolve(root, 'pages/home/index.html'),
+  //       about: resolve(root,  'pages/about/index.html'),
+  //       profile: resolve(root, 'pages/profile/index.html')      }
+  //   }
+  // }
+});
