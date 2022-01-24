f35e6885248d9ad28029573962a4aa78bb9067ae
calamis <mekorn_damboy@yahoo.com>
2022-01-24 22:18:52 +0800

    chore: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// const root = resolve(__dirname, 'src');
-// const outDir = resolve(__dirname, 'dist')
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root,
   plugins: [react()],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+      },
+    },
+  },
 });
