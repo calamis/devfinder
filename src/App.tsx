fd4847c79caaa44e814158647daa20056e0fa0c0
calamis <mekorn_damboy@yahoo.com>
2022-01-27 16:50:50 +0800

    fix: test vercel setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HeaderBar from './Components/Layout/AppBar';
-
 // pages
 import Home from './pages/home/home';
 import Profile from './pages/profile/profile';
