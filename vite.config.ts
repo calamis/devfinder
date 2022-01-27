6682022b3333717f0c203b6feacdd14336754fde
calamis <mekorn_damboy@yahoo.com>
2022-01-27 13:20:51 +0800

    chore: changes vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    jsxFactory: `jsx`,
-    jsxInject: `import { jsx } from '@emotion/react'`,
-  },
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 });
